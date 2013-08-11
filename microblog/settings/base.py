124b56887216ce1bad976c05827b8536a5405a4c
Eddie Welker <eddie.welker@gmail.com>
2013-08-11 17:05:58 +0000

    print statement to print 'DATABASE_URL' env variable

--- microblog/settings/base.py
+++ microblog/settings/base.py
@@ -160,7 +160,9 @@ LOGGING = {
 
 # Parse database configuration from $DATABASE_URL
 import dj_database_url
-DATABASES['default'] =  dj_database_url.config(default=os.environ['DATABASE_URL'])
+default = os.environ['DATABASE_URL']
+print default
+DATABASES['default'] =  dj_database_url.config(default=default)
 
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
